5edd33c56134687255977baef181ee576f3ff5a7
ZOASR <44146518+ZOASR@users.noreply.github.com>
2023-12-30 14:14:38 +0200

    dev: removed project backup files

--- apps/react-lastfm-viewer_/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-import tailwindConfig from "@repo/tailwind-config";
-export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-	presets: [tailwindConfig]
-};
--- apps/solid-lastfm-viewer_/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-import tailwindConfig from "@repo/tailwind-config";
-export default {
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	presets: [tailwindConfig]
-};
