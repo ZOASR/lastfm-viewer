363bdf732fa6ccb102db618f03556f0b05a103fe
ZOASR <44146518+ZOASR@users.noreply.github.com>
2024-02-08 15:05:52 +0200

    fix: removed now unused error strings

--- packages/utils/utils.ts
+++ packages/utils/utils.ts
@@ -1,9 +1,3 @@
-export const unexpectedErrors = [
-	"NetworkError when attempting to fetch resource.",
-	"Login: User required to be logged in",
-	"Failed to fetch"
-];
-
 export const msToMins = (ms: number) => Math.floor(ms / 1000 / 60);
 export const msToSecs = (ms: number) =>
 	Math.floor((ms / 1000) % 60).toLocaleString(undefined, {
