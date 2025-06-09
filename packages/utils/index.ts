ccf87c6d135bdaf9340caa11c1f8f523897c2454
ZOASR <44146518+ZOASR@users.noreply.github.com>
2025-06-09 23:23:11 +0300

    feat(utils): add index file and update package.json exports
    - Created index.ts to consolidate exports from various utility modules.
    - Updated package.json to set the main entry point and types to index.ts.

---
+++ packages/utils/index.ts
@@ -0,0 +1,6 @@
+export { version as APP_VERSION } from "./package.json";
+export * from "./lastfm";
+export * from "./types";
+export type { Release, ReleaseInfo, Image, MBObject } from "./MBtypes";
+export type { UserRecentTracksRes, TrackInfoRes } from "./LFMtypes";
+export * from "./utils";
